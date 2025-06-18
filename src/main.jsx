de16f3890672cc40d1ccebed57d132a1bb275339
ArcLKN <kalidolkn@gmail.com>
2025-06-18 20:19:08 +0200

    Testing routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/cyrcle-website">
-    <App />
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
   </BrowserRouter>
 )
