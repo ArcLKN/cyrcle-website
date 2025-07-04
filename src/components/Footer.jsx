63a0ace2788aea82316ce202b10eabc3de25f4ba
ArcLKN <kalidolkn@gmail.com>
2025-07-04 20:24:17 +0200

    Changed footer a href to Link Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Separator } from "@/components/ui/separator"
+import { Link } from 'react-router-dom'
 
 export function Footer() {
     return <footer className="p-24 border-t text-center">
@@ -8,11 +9,11 @@ export function Footer() {
                 <a href="/" className="text-xl font-bold">Le design qui fait tourner vos projets</a>
             </div>
             <div className="flex flex-col items-start space-y-2">
-                <a href="/" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">Accueil</a>
-                <a href="/" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">Nos Prestations</a>
-                <a href="/" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">Nous Découvrir</a>
-                <a href="/" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">FAQ</a>
-                <a href="/" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">Contact</a>
+                <Link to="/" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">Accueil</Link>
+                <Link to="/prestations" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">Nos Prestations</Link>
+                <Link to="/decouvrir" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">Nous Découvrir</Link>
+                <Link to="/faq" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">FAQ</Link>
+                <Link to="/contact" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">Contact</Link>
             </div>
         </div>
         <Separator className="opacity-30 bg-foreground mb-12 mt-12" />
