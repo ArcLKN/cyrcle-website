7cbd385f30c1cd570556f7e844cc244db2a5d375
ArcLKN <kalidolkn@gmail.com>
2025-07-07 14:43:22 +0200

    Responsive footer sm to xl

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ import { Separator } from "@/components/ui/separator"
 import { Link } from 'react-router-dom'
 
 export function Footer() {
-    return <footer className="p-24 border-t text-center">
-        <div className="flex justify-between space-x-4 mb-4">
+    return <footer className="p-8 md:p-24 border-t text-center">
+        <div className="flex flex-col md:flex-row justify-between space-y-4 space-x-4 mb-4">
             <div className="flex flex-col items-start">
                 <a href="/" className="text-xl font-bold">Cyrcle</a>
-                <a href="/" className="text-xl font-bold">Le design qui fait tourner vos projets</a>
+                <a href="/" className="text-xl font-bold text-left">Le design qui fait tourner vos projets</a>
             </div>
             <div className="flex flex-col items-start space-y-2">
                 <Link to="/" className="opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300">Accueil</Link>
