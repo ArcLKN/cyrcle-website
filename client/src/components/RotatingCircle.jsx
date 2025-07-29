3b8e1a11f5ba7d410b943bec65b0f6925188f94d
ArcLKN <kalidolkn@gmail.com>
2025-07-29 23:56:05 +0200

    Added doAnimate property

--- client/src/components/RotatingCircle.jsx
+++ client/src/components/RotatingCircle.jsx
@@ -2,7 +2,7 @@ import cercleBlanc from "@/assets/cercle blanc.png";
 import cercleNoir from "@/assets/cercle noir.png";
 import { useTheme } from "@/components/theme-provider";
 
-export default function RotatingCircle({ className, delay = "0s" }) {
+export default function RotatingCircle({ className, delay = "0s", doAnimate = true }) {
 
     const { theme, setTheme } = useTheme();
     const cercle = theme === "light" ? cercleNoir : cercleBlanc;
@@ -12,7 +12,7 @@ export default function RotatingCircle({ className, delay = "0s" }) {
             src={cercle}
             alt=""
             aria-hidden="true"
-            className={`pointer-events-none absolute max-w-164 md:max-w-192 lg:max-w-none lg:h-300 w-auto animate-custom-rotate ${className}`}
+            className={`pointer-events-none absolute max-w-164 md:max-w-192 lg:max-w-none lg:h-300 w-auto ${doAnimate ? "animate-custom-rotate" : ""} ${className}`}
             style={{ animationDelay: delay }}
         />
     );
