c136775eb286c522fe5382d67f134badfb014d86
ArcLKN <kalidolkn@gmail.com>
2025-07-30 00:20:38 +0200

    Used the RotatingCircle component

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -1,38 +1,24 @@
-import cercleBlanc from "@/assets/cercle blanc.png";
-import cercleNoir from "@/assets/cercle noir.png";
-import { useTheme } from "@/components/theme-provider";
 import ContactForm from "./ContactForm";
 import RotatingCircle from "@/components/RotatingCircle";
 
 function Contact() {
 
-	const { theme, setTheme } = useTheme();
-	const cercle = theme === "light" ? cercleNoir : cercleBlanc;
-
 	return (
 		<div className='flex flex-col lg:flex-row h-screen w-full items-center space-y-8 lg:space-y-0 lg:space-x-16'>
-			<div className='flex flex-col w-full h-auto lg:w-1/2 lg:h-screen justify-center relative'>
-				<img
-					src={cercle}
-					alt='Cercle blanc'
-					className='pointer-events-none h-180 w-auto absolute -translate-y-32 translate-x-32'
-				/>
-				<h1 className='text-3xl md:text-6xl font-manrope font-extralight'>
+			<section aria-labelledby="contact-heading" className='flex flex-col w-full h-auto lg:w-1/2 lg:h-screen justify-center relative'>
+				<RotatingCircle className="-translate-y-32 translate-x-32 max-w-164 md:max-w-192 lg:max-w-none h-140 lg:h-180" doAnimate={false} />
+				<h1 id="contact-heading" className='text-3xl md:text-6xl font-manrope font-extralight'>
 					CYRCLE :
 				</h1>
-				<p className='text-3xl md:text-6xl font-manrope'>
+				<h2 className='text-3xl md:text-6xl font-manrope'>
 					le design qui fait tourner vos projets !
-				</p>
-				<img
-					src={cercle}
-					alt='Cercle blanc'
-					className='pointer-events-none h-180 w-auto absolute translate-y-40 -translate-x-64'
-				/>
-			</div>
-			<span className='align-middle h-[1px] w-full lg:h-full lg:w-[1px] bg-foreground opacity-50' />
-			<div className='w-full md:w-2/3 lg:1/2 items-center justify-center relative'>
+				</h2>
+				<RotatingCircle className="translate-y-40 -translate-x-64 max-w-164 md:max-w-192 lg:max-w-none h-140 lg:h-180" doAnimate={false} />
+			</section>
+			<span className='align-middle h-[1px] w-full lg:h-full lg:w-[1px] bg-foreground opacity-50' aria-hidden="true" />
+			<section aria-labelledby="contact-form" className='w-full md:w-2/3 lg:1/2 items-center justify-center relative'>
 				<ContactForm />
-			</div>
+			</section>
 		</div>
 	);
 }
