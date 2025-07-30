bfdedb65e6eb8ec23ab62c50b0507f26fc57b8b6
ArcLKN <kalidolkn@gmail.com>
2025-07-30 15:37:42 +0200

    Ajout des textes de la FAQ

--- client/src/pages/FAQ.jsx
+++ client/src/pages/FAQ.jsx
@@ -25,10 +25,8 @@ function FAQ() {
 						Comment nous contacter ?
 					</AccordionTrigger>
 					<AccordionContent className='flex flex-col gap-4 text-balance'>
-						Vitae congue eu consequat ac felis placerat vestibulum
-						lectus mauris ultrices. Cursus sit amet dictum sit amet
-						justo donec enim diam porttitor lacus luctus accumsan
-						tortor posuere.
+						<p>À travers notre page de contact, notre adresse mail <a href="mailto:cyrcle.cydesign@gmail.com">cyrcle.cydesign@gmail.com</a> ou encore notre <a href="https://www.linkedin.com/company/cyrcle-design/">compte linkedin</a>.
+							Nous sommes disponibles pour réponde à vos questionnements et vos potentiels projets.</p>
 					</AccordionContent>
 				</AccordionItem>
 
@@ -37,10 +35,8 @@ function FAQ() {
 						Où sommes-nous situés ?
 					</AccordionTrigger>
 					<AccordionContent className='flex flex-col gap-4 text-balance'>
-						Vitae congue eu consequat ac felis placerat vestibulum
-						lectus mauris ultrices. Cursus sit amet dictum sit amet
-						justo donec enim diam porttitor lacus luctus accumsan
-						tortor posuere.
+						Notre école est située à Saint germain en laye à IxCampus, un campus mêlant entreprise et étudiant.
+						Nous pouvons nous déplacer pour certaines missions.
 					</AccordionContent>
 				</AccordionItem>
 
@@ -49,10 +45,9 @@ function FAQ() {
 						Quels services proposons-nous ?
 					</AccordionTrigger>
 					<AccordionContent className='flex flex-col gap-4 text-balance'>
-						Vitae congue eu consequat ac felis placerat vestibulum
-						lectus mauris ultrices. Cursus sit amet dictum sit amet
-						justo donec enim diam porttitor lacus luctus accumsan
-						tortor posuere.
+						Nous accompagnons entreprises et collectivités dans leurs projets d’innovation grâce à une approche pluridisciplinaire.
+						Nos prestations incluent la stratégie d’expérience, le design industriel, l’identité de marque (graphisme, digital, développement web), le design d’espace et le design événementiel.
+						Nous animons également des workshops sur mesure et réalisons des audits pour répondre à des problématiques internes et orienter les prises de décision.
 					</AccordionContent>
 				</AccordionItem>
 
@@ -61,10 +56,9 @@ function FAQ() {
 						Quels sont nos tarifs ? Offrons-nous des réductions ?
 					</AccordionTrigger>
 					<AccordionContent className='flex flex-col gap-4 text-balance'>
-						Vitae congue eu consequat ac felis placerat vestibulum
-						lectus mauris ultrices. Cursus sit amet dictum sit amet
-						justo donec enim diam porttitor lacus luctus accumsan
-						tortor posuere.
+						Nos tarifs sont mis en place en accord avec votre projet en fonction d’une grille tarifaire établie pour rémunérer nos étudiants à la hauteur de leurs travails.
+						Un budget final est réalisé en collaboration avec nos membres pour répondre au mieux à votre problématique.
+						Pour cette raison, nous ne proposons aucune réduction.
 					</AccordionContent>
 				</AccordionItem>
 
@@ -73,10 +67,9 @@ function FAQ() {
 						Quelle est l'histoire de notre jeune entreprise ?
 					</AccordionTrigger>
 					<AccordionContent className='flex flex-col gap-4 text-balance'>
-						Vitae congue eu consequat ac felis placerat vestibulum
-						lectus mauris ultrices. Cursus sit amet dictum sit amet
-						justo donec enim diam porttitor lacus luctus accumsan
-						tortor posuere.
+						Née de l’initiative de trois étudiants de CY école de design,  Lou Gicquel, Appoline Boiselle et Mathis Dufal,  notre junior entreprise est le fruit d’une volonté commune d’ajouter une dimension entrepreneuriale à leur formation d’ingénieur-designer.
+						Avec l’ambition de faire grandir les talents de l’école et de renforcer ses liens avec le monde professionnel, ils fondent CYRCLE design.
+						Un an plus tard, l'association intègre officiellement le mouvement des Junior-Entreprises et propose désormais des missions rétribuées aux étudiants, favorisant leur montée en compétences et le rayonnement de l’école.
 					</AccordionContent>
 				</AccordionItem>
 
@@ -85,10 +78,10 @@ function FAQ() {
 						Quels sont les délais de nos prestations ?
 					</AccordionTrigger>
 					<AccordionContent className='flex flex-col gap-4 text-balance'>
-						Vitae congue eu consequat ac felis placerat vestibulum
-						lectus mauris ultrices. Cursus sit amet dictum sit amet
-						justo donec enim diam porttitor lacus luctus accumsan
-						tortor posuere.
+						Nos délais s’adaptent aux besoins et aux contraintes de votre entreprise, dans la mesure du possible.
+						Toutefois, en tant que junior entreprise, nos consultants sont des étudiants qui mènent leurs missions en parallèle de leurs études.
+						Cela signifie qu’ils ne peuvent pas travailler à temps plein, ce qui peut rallonger certains délais.
+						Nous veillons cependant à proposer un calendrier réaliste et à respecter les échéances fixées ensemble.
 					</AccordionContent>
 				</AccordionItem>
 			</Accordion>
