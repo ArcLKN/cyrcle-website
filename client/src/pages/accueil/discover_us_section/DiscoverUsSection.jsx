21e66603147ab14a367fd9e2d00153c81bf75bc5
ArcLKN <kalidolkn@gmail.com>
2025-08-05 12:05:06 +0200

    Added missing rotatinc circle

--- client/src/pages/accueil/discover_us_section/DiscoverUsSection.jsx
+++ client/src/pages/accueil/discover_us_section/DiscoverUsSection.jsx
@@ -3,43 +3,54 @@ import { Link } from "react-router-dom";
 import RotatingCircle from "@/components/RotatingCircle";
 
 export default function DiscoverUsSection() {
-
-    return (
-        <section aria-labelledby="discover-us-heading" className="relative">
-            <RotatingCircle className='-right-0 translate-x-92 -translate-y-32' delay="9s" />
-            <div className='flex flex-col lg:flex-row items-start lg:items-center mt-42 mb-32 space-x-12'>
-                <div className='text-right hidden lg:flex flex-row lg:flex-col space-y-3'>
-                    <Link
-                        to='/'
-                        className='opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300'
-                    >
-                        LA TEAM
-                    </Link>
-                    <Link
-                        to='/'
-                        className='opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300'
-                    >
-                        NOTRE ESPRIT
-                    </Link>
-                </div>
-                <h1 id='discover-us-heading' className='text-5xl md:text-7xl lg:text-9xl font-manrope'>NOUS DÉCOUVRIR</h1>
-                <div className='flex lg:hidden flex-row lg:flex-col space-x-4'>
-                    <Link
-                        to='/'
-                        className='opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300'
-                    >
-                        LA TEAM
-                    </Link>
-                    <Link
-                        to='/'
-                        className='opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300'
-                    >
-                        NOTRE ESPRIT
-                    </Link>
-                </div>
-                <span className='hidden lg:flex ml-10 align-middle w-96 h-[3px] bg-foreground translate-x-24' />
-            </div>
-            <TeamProfilesGrids />
-        </section>
-    )
+	return (
+		<section aria-labelledby='discover-us-heading' className='relative'>
+			<RotatingCircle
+				className='-right-0 translate-x-92 -translate-y-32'
+				delay='9s'
+			/>
+			<div className='flex flex-col lg:flex-row items-start lg:items-center mt-42 mb-32 space-x-12'>
+				<div className='text-right hidden lg:flex flex-row lg:flex-col space-y-3'>
+					<Link
+						to='/'
+						className='opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300'
+					>
+						LA TEAM
+					</Link>
+					<Link
+						to='/'
+						className='opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300'
+					>
+						NOTRE ESPRIT
+					</Link>
+				</div>
+				<h1
+					id='discover-us-heading'
+					className='text-5xl md:text-7xl lg:text-9xl font-manrope'
+				>
+					NOUS DÉCOUVRIR
+				</h1>
+				<div className='flex lg:hidden flex-row lg:flex-col space-x-4'>
+					<Link
+						to='/'
+						className='opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300'
+					>
+						LA TEAM
+					</Link>
+					<Link
+						to='/'
+						className='opacity-50 hover:opacity-100 hover:scale-105 transition-all duration-300'
+					>
+						NOTRE ESPRIT
+					</Link>
+				</div>
+				<span className='hidden lg:flex ml-10 align-middle w-96 h-[3px] bg-foreground translate-x-24' />
+			</div>
+			<TeamProfilesGrids />
+			<RotatingCircle
+				className='left-0 -translate-x-84 -translate-y-256'
+				delay='1s'
+			/>
+		</section>
+	);
 }
