32e4436f3688c06ee79dc5fab2cf0137d833c0ba
ArcLKN <kalidolkn@gmail.com>
2025-08-22 15:36:51 +0200

    test

--- functions/submit.js
+++ functions/submit.js
@@ -1,28 +1,20 @@
 export async function onRequestPost(context) {
-  // Extract form data
   const data = await context.request.formData();
 
-  // Add your secret key from environment variables
-  data.append("access_key", "1e4dc2df-1659-4610-9c21-63cb83781d2a");
-  console.log("Key:", data);
-
   const dataToSend = new FormData();
-dataToSend.append("access_key", "1e4dc2df-1659-4610-9c21-63cb83781d2a");
-dataToSend.append("name", data.nom);
-dataToSend.append("email", data.email);
-dataToSend.append("message", data.message);
-dataToSend.append("subject", data.objet || `${data.nom} sent a message`);
+  dataToSend.append("access_key", "YOUR_KEY_HERE");
+  dataToSend.append("name", data.get("nom"));
+  dataToSend.append("email", data.get("email"));
+  dataToSend.append("message", data.get("message"));
+  dataToSend.append("subject", data.get("objet") || `${data.get("nom")} sent a message`);
 
-  // Forward the request to Web3Forms
   const response = await fetch("https://api.web3forms.com/submit", {
     method: "POST",
     body: dataToSend,
   });
 
-  // Parse JSON from Web3Forms
   const result = await response.json();
 
-  // Return JSON to your frontend
   return new Response(JSON.stringify(result), {
     status: response.status,
     headers: { "Content-Type": "application/json" },
