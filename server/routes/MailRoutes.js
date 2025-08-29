8b9b052aade063533390476d1a533178d1574503
ArcLKN <kalidolkn@gmail.com>
2025-08-30 01:22:23 +0200

    Not using any backend for now

--- server/routes/MailRoutes.js
+++
@@ -1,9 +0,0 @@
-// server/routes/MailRoutes.js
-const express = require("express");
-const { sendMailtoCyrcle } = require("../controllers/mailController");
-const router = express.Router();
-const mailLimiter = require("../middleware/mailLimiter");
-
-router.post("/email", mailLimiter, sendMailtoCyrcle);
-
-module.exports = router;
