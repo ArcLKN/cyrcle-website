28c461499e2d1851fbcfc16b1469d5c268e78f6c
ArcLKN <kalidolkn@gmail.com>
2025-08-30 01:18:14 +0200

    Removing useless log

--- client/src/pages/accueil/discover_us_section/TeamProfilesGrid.jsx
+++ client/src/pages/accueil/discover_us_section/TeamProfilesGrid.jsx
@@ -2,8 +2,6 @@ import TeamProfileCard from "./TeamProfileCard";
 import { teamProfiles } from "@/data/teamProfiles";
 
 export default function TeamProfilesGrids() {
-	console.log(teamProfiles);
-
 	return (
 		<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 items-center justify-items-center'>
 			{teamProfiles.map((infos, index) => (
